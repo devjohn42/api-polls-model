d3dc1c3caeb33dab6d79d84498922146e0428f4f
devjohn42 <devjohncb@gmail.com>
2025-08-23 19:19:09 -0300

    :gear: config: configure the prisma

---
+++ prisma/migrations/20250823221633_create_poll/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "public"."poll" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "poll_pkey" PRIMARY KEY ("id")
+);
